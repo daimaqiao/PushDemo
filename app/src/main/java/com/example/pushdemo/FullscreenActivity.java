f2eaf9f04e942bb95b8cce533d4f77191c68efeb
daimaqiao <daimaqiao@126.com>
2020-07-04 12:36:33 +0800

    添加bps/fps统计

--- app/src/main/java/com/example/pushdemo/FullscreenActivity.java
+++ app/src/main/java/com/example/pushdemo/FullscreenActivity.java
@@ -2,6 +2,7 @@ package com.example.pushdemo;
 
 import android.Manifest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -22,6 +23,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.faucamp.simplertmp.RtmpHandler;
@@ -35,6 +37,9 @@ import net.ossrs.yasea.SrsRecordHandler;
 
 import java.io.IOException;
 import java.net.SocketException;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -50,12 +55,15 @@ public class FullscreenActivity extends AppCompatActivity implements RtmpHandler
     private static final String KEY_URL = "rtmpUrl";
     private static final String TEXT_PUBLISH = "publish";
     private static final String TEXT_UNPUBLISH = "stop";
+    private static final String TEXT_HARD_ENCODER= "hard encoder";
+    private static final String TEXT_SOFT_ENCODER= "soft encoder";
 
     private EditText editUrl;
     private Button btnLive;
     private Button btnScan;
     private Button btnPublish;
     private Button btnSwitchCamera;
+    private Button btnEncoder;
 
     private SharedPreferences sp;
 
@@ -68,11 +76,68 @@ public class FullscreenActivity extends AppCompatActivity implements RtmpHandler
     private int mHeight = 720;
     private boolean isPermissionGranted = false;
 
-    private void hideActionBar() {
+    private final Handler statHandler = new Handler();
+    private TextView textNet, textFps;
+    private double bpsAudio, bpsVideo, fpsVideo;
+    private void initStat() {
+        textNet = (TextView) findViewById(R.id.text_net);
+        textFps = (TextView) findViewById(R.id.text_fps);
+
+        textNet.setText("");
+        textFps.setText("");
+
+        bpsAudio = bpsVideo = fpsVideo = 0;
+    }
+
+    private final Runnable statUpdate = new Runnable() {
+        @Override
+        public void run() {
+            int bps = (int) (bpsAudio + bpsVideo);
+            int fps = (int) fpsVideo;
+            String unit = "";
+            if (bps >= 1024) {
+                unit = "k";
+                bps /= 1024;
+            }
+            String outNet = String.format(Locale.getDefault(),
+                    "bps: %d%s", bps, unit);
+            String outFps = String.format(Locale.getDefault(),
+                    "fps: %d", fps);
+
+            textNet.setText(outNet);
+            textFps.setText(outFps);
+
+            Log.i(TAG, String.format("%s, %s", outNet, outFps));
+
+            statHandler.postDelayed(statUpdate, 3000);
+        }
+    };
+
+    private void startStat() {
+        bpsAudio = bpsVideo = fpsVideo = 0;
+
+        statHandler.removeCallbacks(statUpdate);
+        statHandler.postDelayed(statUpdate, 3000);
+    }
+
+    private void stopStat() {
+        statHandler.removeCallbacks(statUpdate);
+        statHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                textNet.setText("");
+                textFps.setText("");
+            }
+        });
+    }
+
+    private void setupFullscreen() {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.hide();
         }
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
     private void keepScreenOn() {
@@ -150,6 +215,70 @@ public class FullscreenActivity extends AppCompatActivity implements RtmpHandler
             Toast.makeText(this, "Bad URL - " + url, Toast.LENGTH_SHORT).show();
     }
 
+    private void startPublish() {
+        rtmpUrl = editUrl.getText().toString();
+        if (rtmpUrl.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Bad URL!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString(KEY_URL, rtmpUrl);
+        editor.apply();
+
+        promptEncoder();
+
+        mPublisher.startPublish(rtmpUrl);
+        mPublisher.startCamera();
+
+        btnPublish.setText(TEXT_UNPUBLISH);
+        closeScreenOrientation();
+
+        btnEncoder.setEnabled(false);
+        showLiveWarn();
+        enableQrScan(false);
+        delayedHide(AUTO_HIDE_DELAY_MILLIS);
+        startStat();
+    }
+
+    private void stopPublish() {
+        openScreenOrientation();
+
+        mPublisher.stopPublish();
+        mPublisher.startCamera();
+
+        btnPublish.setText(TEXT_PUBLISH);
+
+        btnEncoder.setEnabled(true);
+        hideLive();
+        enableQrScan(true);
+        stopStat();
+    }
+
+    private void resetPublish() {
+
+        stopPublish();
+
+    }
+
+    private void promptEncoder() {
+        String encoder;
+        switch(btnEncoder.getText().toString()) {
+            case TEXT_SOFT_ENCODER:
+                encoder = TEXT_HARD_ENCODER;
+                break;
+
+            case TEXT_HARD_ENCODER:
+                encoder = TEXT_SOFT_ENCODER;
+                break;
+
+            default:
+                encoder = "undefined encoder";
+                break;
+        }// switch
+        Toast.makeText(getApplicationContext(), "Using " + encoder, Toast.LENGTH_SHORT).show();
+    }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -174,6 +303,7 @@ public class FullscreenActivity extends AppCompatActivity implements RtmpHandler
         btnLive = (Button) findViewById(R.id.button_live);
         btnPublish = (Button) findViewById(R.id.button_publish);
         btnSwitchCamera = (Button) findViewById(R.id.button_switch);
+        btnEncoder = (Button) findViewById(R.id.button_encoder);
         mCameraView = (SrsCameraView) findViewById(R.id.camera_view);
 
         hideLive();
@@ -186,7 +316,13 @@ public class FullscreenActivity extends AppCompatActivity implements RtmpHandler
         mPublisher.setPreviewResolution(mWidth, mHeight);
         mPublisher.setOutputResolution(mHeight, mWidth);
         mPublisher.setVideoHDMode();
+        mPublisher.setVideoFpsLower();
+        mPublisher.setScreenOrientation(getResources().getConfiguration().orientation);
         mPublisher.startCamera();
+
+        btnPublish.setText(TEXT_PUBLISH);
+        btnEncoder.setText(TEXT_SOFT_ENCODER);
+        mPublisher.switchToHardEncoder();
 
         mCameraView.setCameraCallbacksHandler(new SrsCameraView.CameraCallbacksHandler() {
             @Override
@@ -199,40 +335,13 @@ public class FullscreenActivity extends AppCompatActivity implements RtmpHandler
             @Override
             public void onClick(View v) {
                 if (btnPublish.getText().toString().contentEquals(TEXT_PUBLISH)) {
-                    rtmpUrl = editUrl.getText().toString();
-                    if (rtmpUrl.isEmpty()) {
-                        Toast.makeText(getApplicationContext(), "Bad URL!", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
 
-                    SharedPreferences.Editor editor = sp.edit();
-                    editor.putString(KEY_URL, rtmpUrl);
-                    editor.apply();
-
-                    mPublisher.switchToHardEncoder();
-
-                    mPublisher.startPublish(rtmpUrl);
-                    mPublisher.startCamera();
-
-                    btnPublish.setText(TEXT_UNPUBLISH);
-                    closeScreenOrientation();
-
-                    showLiveWarn();
-                    enableQrScan(false);
-                    delayedHide(AUTO_HIDE_DELAY_MILLIS);
+                    startPublish();
 
                 } else if (btnPublish.getText().toString().contentEquals(TEXT_UNPUBLISH)) {
 
-                    openScreenOrientation();
+                    stopPublish();
 
-                    mPublisher.stopPublish();
-                    mPublisher.stopRecord();
-                    mPublisher.startCamera();
-
-                    btnPublish.setText(TEXT_PUBLISH);
-
-                    hideLive();
-                    enableQrScan(true);
                 }
             }
         });
@@ -241,6 +350,19 @@ public class FullscreenActivity extends AppCompatActivity implements RtmpHandler
             @Override
             public void onClick(View v) {
                 mPublisher.switchCameraFace((mPublisher.getCameraId() + 1) % Camera.getNumberOfCameras());
+            }
+        });
+
+        btnEncoder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btnEncoder.getText().toString().contentEquals(TEXT_SOFT_ENCODER)) {
+                    mPublisher.switchToSoftEncoder();
+                    btnEncoder.setText(TEXT_HARD_ENCODER);
+                } else if (btnEncoder.getText().toString().contentEquals(TEXT_HARD_ENCODER)) {
+                    mPublisher.switchToHardEncoder();
+                    btnEncoder.setText(TEXT_SOFT_ENCODER);
+                }
             }
         });
 
@@ -276,55 +398,54 @@ public class FullscreenActivity extends AppCompatActivity implements RtmpHandler
 
 
     private void handleException(Exception e) {
+        Log.e(TAG, "handle exception", e);
         try {
             Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-            mPublisher.stopPublish();
-            mPublisher.stopRecord();
-            btnPublish.setText(TEXT_PUBLISH);
-            hideLive();
-            enableQrScan(true);
-            mPublisher.startCamera();
-        } catch (Exception e1) {
-            //
+
+            resetPublish();
+
+        } catch (Exception e2) {
+            Log.e(TAG, "handle exception 2", e2);
         }
     }
 
 
     @Override
     protected void onStart() {
+        Log.i(TAG, "onStart");
         super.onStart();
+
         if(mPublisher.getCamera() == null && isPermissionGranted){
-            //if the camera was busy and available again
             mPublisher.startCamera();
         }
     }
 
     @Override
     protected void onResume() {
+        Log.i(TAG, "onResume");
         super.onResume();
-        final Button btn = (Button) findViewById(R.id.button_publish);
-        btn.setEnabled(true);
-        mPublisher.resumeRecord();
     }
 
     @Override
     protected void onPause() {
+        Log.i(TAG, "onPause");
         super.onPause();
-        mPublisher.pauseRecord();
     }
 
     @Override
     protected void onDestroy() {
+        Log.i(TAG, "onDestory");
         super.onDestroy();
-        mPublisher.stopPublish();
-        mPublisher.stopRecord();
+
+        resetPublish();
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        Log.i(TAG, "onConfigurationChanged orientation=" + newConfig.orientation);
+
         super.onConfigurationChanged(newConfig);
         mPublisher.stopEncode();
-        mPublisher.stopRecord();
         mPublisher.setScreenOrientation(newConfig.orientation);
         if (btnPublish.getText().toString().contentEquals(TEXT_UNPUBLISH)) {
             mPublisher.startEncode();
@@ -393,7 +514,7 @@ public class FullscreenActivity extends AppCompatActivity implements RtmpHandler
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        hideActionBar();
+        setupFullscreen();
         keepScreenOn();
 
         setContentView(R.layout.activity_fullscreen);
@@ -410,20 +531,23 @@ public class FullscreenActivity extends AppCompatActivity implements RtmpHandler
 
         openScreenOrientation();
         requestPermission();
+
+        initStat();
     }
 
     ////////////////////////////////////////
     // interface RtmpListener
     @Override
     public void onRtmpConnecting(String s) {
-        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "on rtmp connecting");
     }
 
     // interface RtmpListener
     @Override
     public void onRtmpConnected(String s) {
+        Log.i(TAG, "on rtmp connected " + s);
+
         showLiveOk();
-        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
     }
 
     // interface RtmpListener
@@ -439,25 +563,31 @@ public class FullscreenActivity extends AppCompatActivity implements RtmpHandler
     // interface RtmpListener
     @Override
     public void onRtmpStopped() {
-        Toast.makeText(getApplicationContext(), "Stopped", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "on rtmp stopped");
     }
 
     // interface RtmpListener
     @Override
     public void onRtmpDisconnected() {
+        Log.i(TAG, "on rtmp disconnected");
+
         hideLive();
-        Toast.makeText(getApplicationContext(), "Disconnected", Toast.LENGTH_SHORT).show();
+        stopStat();
     }
 
     // interface RtmpListener
     @Override
     public void onRtmpVideoFpsChanged(double v) {
+        fpsVideo = v;
+
         Log.i(TAG, String.format("Output Fps: %f", v));
     }
 
     // interface RtmpListener
     @Override
     public void onRtmpVideoBitrateChanged(double v) {
+        bpsVideo = v;
+
         int rate = (int) v;
         if (rate / 1000 > 0) {
             Log.i(TAG, String.format("Video bitrate: %f kbps", v / 1000));
@@ -469,6 +599,8 @@ public class FullscreenActivity extends AppCompatActivity implements RtmpHandler
     // interface RtmpListener
     @Override
     public void onRtmpAudioBitrateChanged(double v) {
+        bpsAudio = v;
+
         int rate = (int) v;
         if (rate / 1000 > 0) {
             Log.i(TAG, String.format("Audio bitrate: %f kbps", v / 1000));
@@ -539,15 +671,17 @@ public class FullscreenActivity extends AppCompatActivity implements RtmpHandler
     // interface SrsEncodeListener
     @Override
     public void onNetworkWeak() {
-        showLiveWarn();
-        Toast.makeText(getApplicationContext(), "Network weak", Toast.LENGTH_SHORT).show();
+        Log.e(TAG, "on network weak");
+
+        showLiveError();
     }
 
     // interface SrsEncodeListener
     @Override
     public void onNetworkResume() {
+        Log.e(TAG, "on network resume");
+
         showLiveOk();
-        Toast.makeText(getApplicationContext(), "Network resume", Toast.LENGTH_SHORT).show();
     }
 
     // interface SrsEncodeListener
